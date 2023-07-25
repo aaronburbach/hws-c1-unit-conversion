c96959728604f2aadb496dddf4366e4fa24ea0c7
Aaron Burbach <aaronburbach@cattlekrush.com>
2023-07-25 17:07:44 -0500

    Initial iOS project creation.

---
+++ HWS Challenge Unit Converter/HWS Challenge Unit Converter/HWS_Challenge_Unit_ConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  HWS_Challenge_Unit_ConverterApp.swift
+//  HWS Challenge Unit Converter
+//
+//  Created by Aaron Burbach on 7/25/23.
+//
+
+import SwiftUI
+
+@main
+struct HWS_Challenge_Unit_ConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
